a3be979ce4a8f90d3d8d64c0f4c1aa6ed7ad9eaf
Sofia <0255@htl.rennweg.at>
2023-12-11 17:42:06 +0100

    move dirs

---
+++ PYTHON/01-useranlegen/user_script.sh
@@ -0,0 +1,117 @@
+useradd -d /home/students/de_junious -c de_junious -m -g 2CN -G cdrom,plugdev,sambashare -s /bin/bash de_junious
+echo de_junious:a\-\(\=\_\_\^ | chpasswd
+
+useradd -d /home/students/nunez_gomez -c nunez_gomez -m -g 3BN -G cdrom,plugdev,sambashare -s /bin/bash nunez_gomez
+echo nunez_gomez:a\.\.\=\^\(\^ | chpasswd
+
+useradd -d /home/students/uellaegoess -c uellaegoess -m -g 1AN -G cdrom,plugdev,sambashare -s /bin/bash uellaegoess
+echo uellaegoess:a\-\_\-\&\!\( | chpasswd
+
+useradd -d /home/students/lanpher -c lanpher -m -g 4CN -G cdrom,plugdev,sambashare -s /bin/bash lanpher
+echo lanpher:a\#\^\!\-\%\( | chpasswd
+
+useradd -d /home/students/galvin -c galvin -m -g 2AN -G cdrom,plugdev,sambashare -s /bin/bash galvin
+echo galvin:a\(\%\,\)\,\. | chpasswd
+
+useradd -d /home/students/laspina -c laspina -m -g 4CN -G cdrom,plugdev,sambashare -s /bin/bash laspina
+echo laspina:a\#\=\)\!\.\) | chpasswd
+
+useradd -d /home/students/dodich -c dodich -m -g 3CN -G cdrom,plugdev,sambashare -s /bin/bash dodich
+echo dodich:a\,\_\-\_\#\% | chpasswd
+
+useradd -d /home/students/bannowsky -c bannowsky -m -g 2AN -G cdrom,plugdev,sambashare -s /bin/bash bannowsky
+echo bannowsky:a\!\!\%\#\,\. | chpasswd
+
+useradd -d /home/students/waisath -c waisath -m -g 4CN -G cdrom,plugdev,sambashare -s /bin/bash waisath
+echo waisath:a\-\=\-\,\(\) | chpasswd
+
+useradd -d /home/students/von_reddig-piette -c von_reddig-piette -m -g 2CN -G cdrom,plugdev,sambashare -s /bin/bash von_reddig-piette
+echo von_reddig-piette:a\,\#\(\&\,\. | chpasswd
+
+useradd -d /home/students/latona -c latona -m -g 2AN -G cdrom,plugdev,sambashare -s /bin/bash latona
+echo latona:a\=\#\(\%\%\% | chpasswd
+
+useradd -d /home/students/homewood -c homewood -m -g 2BN -G cdrom,plugdev,sambashare -s /bin/bash homewood
+echo homewood:a\(\.\-\-\-\& | chpasswd
+
+useradd -d /home/students/pallafor-zedian -c pallafor-zedian -m -g 5CN -G cdrom,plugdev,sambashare -s /bin/bash pallafor-zedian
+echo pallafor-zedian:a\)\,\&\!\,\! | chpasswd
+
+useradd -d /home/students/senemounnarat-quillian -c senemounnarat-quillian -m -g 2CN -G cdrom,plugdev,sambashare -s /bin/bash senemounnarat-quillian
+echo senemounnarat-quillian:a\)\!\#\)\^\) | chpasswd
+
+useradd -d /home/students/isaac -c isaac -m -g 2BN -G cdrom,plugdev,sambashare -s /bin/bash isaac
+echo isaac:a\(\!\&\-\)\& | chpasswd
+
+useradd -d /home/students/plancarte -c plancarte -m -g 4BN -G cdrom,plugdev,sambashare -s /bin/bash plancarte
+echo plancarte:a\%\)\!\!\.\_ | chpasswd
+
+useradd -d /home/teachers/berteotti-stirn -c berteotti-stirn -m -g teacher -G cdrom,plugdev,sambashare -s /bin/bash berteotti-stirn
+echo berteotti-stirn:a\=\.\%\.\(\. | chpasswd
+
+useradd -d /home/students/strycker -c strycker -m -g 3AN -G cdrom,plugdev,sambashare -s /bin/bash strycker
+echo strycker:a\,\#\#\.\!\^ | chpasswd
+
+useradd -d /home/students/von_verrill -c von_verrill -m -g 3BN -G cdrom,plugdev,sambashare -s /bin/bash von_verrill
+echo von_verrill:a\.\,\&\(\(\_ | chpasswd
+
+useradd -d /home/students/deschner -c deschner -m -g 4CN -G cdrom,plugdev,sambashare -s /bin/bash deschner
+echo deschner:a\!\-\)\=\!\& | chpasswd
+
+useradd -d /home/teachers/franzi -c franzi -m -g teacher -G cdrom,plugdev,sambashare -s /bin/bash franzi
+echo franzi:a\-\&\!\,\%\! | chpasswd
+
+useradd -d /home/students/cringle -c cringle -m -g 3CN -G cdrom,plugdev,sambashare -s /bin/bash cringle
+echo cringle:a\&\#\#\^\=\) | chpasswd
+
+useradd -d /home/students/kosh -c kosh -m -g 5BN -G cdrom,plugdev,sambashare -s /bin/bash kosh
+echo kosh:a\!\&\!\&\-\& | chpasswd
+
+useradd -d /home/students/derouchie -c derouchie -m -g 4BN -G cdrom,plugdev,sambashare -s /bin/bash derouchie
+echo derouchie:a\=\^\^\)\-\^ | chpasswd
+
+useradd -d /home/students/doutt -c doutt -m -g 5BN -G cdrom,plugdev,sambashare -s /bin/bash doutt
+echo doutt:a\%\^\#\,\^\= | chpasswd
+
+useradd -d /home/students/munley -c munley -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash munley
+echo munley:a\#\=\=\_\!\_ | chpasswd
+
+useradd -d /home/students/dornbos -c dornbos -m -g 5AN -G cdrom,plugdev,sambashare -s /bin/bash dornbos
+echo dornbos:a\_\&\!\,\,\# | chpasswd
+
+useradd -d /home/students/mahala -c mahala -m -g 2CN -G cdrom,plugdev,sambashare -s /bin/bash mahala
+echo mahala:a\=\!\=\!\!\# | chpasswd
+
+useradd -d /home/teachers/ellingwood -c ellingwood -m -g teacher -G cdrom,plugdev,sambashare -s /bin/bash ellingwood
+echo ellingwood:a\(\)\(\(\!\- | chpasswd
+
+useradd -d /home/students/de_cardinalli-sciola -c de_cardinalli-sciola -m -g 3AN -G cdrom,plugdev,sambashare -s /bin/bash de_cardinalli-sciola
+echo de_cardinalli-sciola:a\(\%\.\%\#\. | chpasswd
+
+useradd -d /home/teachers/gundry -c gundry -m -g teacher -G cdrom,plugdev,sambashare -s /bin/bash gundry
+echo gundry:a\=\&\%\&\_\_ | chpasswd
+
+useradd -d /home/students/pinedo -c pinedo -m -g 3CN -G cdrom,plugdev,sambashare -s /bin/bash pinedo
+echo pinedo:a\%\=\!\^\!\^ | chpasswd
+
+useradd -d /home/students/faivre -c faivre -m -g 2CN -G cdrom,plugdev,sambashare -s /bin/bash faivre
+echo faivre:a\(\!\^\(\=\- | chpasswd
+
+useradd -d /home/students/pinsky -c pinsky -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash pinsky
+echo pinsky:a\!\=\^\(\,\_ | chpasswd
+
+useradd -d /home/students/pinsky1 -c pinsky1 -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash pinsky1
+echo pinsky1:a\#\,\(\%\&\_ | chpasswd
+
+useradd -d /home/students/pinsky2 -c pinsky2 -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash pinsky2
+echo pinsky2:a\-\=\(\%\&\, | chpasswd
+
+useradd -d /home/students/pinsky3 -c pinsky3 -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash pinsky3
+echo pinsky3:a\(\=\!\(\#\& | chpasswd
+
+useradd -d /home/students/norlien -c norlien -m -g 2AN -G cdrom,plugdev,sambashare -s /bin/bash norlien
+echo norlien:a\!\&\-\^\%\^ | chpasswd
+
+useradd -d /home/students/preato -c preato -m -g 1BN -G cdrom,plugdev,sambashare -s /bin/bash preato
+echo preato:a\(\)\!\_\.\, | chpasswd
+
